4a5f51c357a1b54fb89783313c15be5b6e77ef62
Tony Eneh <aeneh90@gmail.com>
2020-05-26 16:47:34 +0100

    feat: integrate pug view template

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ const port = process.env.PORT || 8000;
  *  App Configuration
  */
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 /**
  * Routes Definitions
  */
 
 app.use('/', (req,res)=>{
-    res.status(200).send('WHATABYTE: Food For Devs');
+    res.render('index', {title: 'Home'});
 })
 
 /**
